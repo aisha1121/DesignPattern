70cfe90c03e1bf676d89da1096b8e1659c577be5
Aayusha <aayushasrivastava1121@gmail.com>
2025-04-29 00:05:10 +0530

    logs added

--- src/main/java/DesignPattern/CreationalDesignPattern/FactoryDesignPattern/Main.java
+++ src/main/java/DesignPattern/CreationalDesignPattern/FactoryDesignPattern/Main.java
@@ -3,6 +3,10 @@ package DesignPattern.CreationalDesignPattern.FactoryDesignPattern;
 /*
     * You don't know in advance which exact class you need to create
     * Factory decides at runtime which object to instantiate
+    * suppose circle class is instantiated in 100 classes on the basis of some logic.
+    * Now in future if we want to change the condition on the basis of which circle is selected,
+    * then in all 100 classes we need to update the logic. Factory pattern helps here as we can update
+    * logic in a single class
  */
 public class Main {
     public static void main(String[] args) {
